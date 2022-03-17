fcd78607a39ce3c25febda087c60778a0c2aad2a
Klajdi <klajdixhafkollari36@gmail.com>
2022-03-17 11:58:05 +0100

    final

--- src/Pages/Favorites/Favorites.js
+++ src/Pages/Favorites/Favorites.js
@@ -1,14 +1,12 @@
 import { useContext } from 'react';
 import FavoritesContext from '../../store/favorites-context';
 import MovieItem from '../../components/Movie/MovieItem';
-import classes from "./Favorites.module.css";
-
 
 const Favorites = () => {
     const favoritesCtx = useContext(FavoritesContext);
     
     return(
-        <section className={classes.section}>
+        <section>
             <h1>Favorites Movies</h1>
             {favoritesCtx.totalFavorites === 0 ? (
                 <p> You got no movies yet. Start adding some movies</p>
--- src/Pages/Later/Later.js
+++ src/Pages/Later/Later.js
@@ -1,14 +1,13 @@
 import { useContext } from 'react';
 import LaterContext from '../../store/later-context';
 import MovieItem from '../../components/Movie/MovieItem';
-import classes from "./Later.module.css";
 
 
 const Later = () => {
     const laterCtx = useContext(LaterContext);
     
     return(
-        <section className={classes.section}>
+        <section>
             <h1>Watch Later Movies</h1>
             {laterCtx.totalLater === 0 ? (
                 <p> You got no movies yet. Start adding some movies</p>
