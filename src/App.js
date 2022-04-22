fe1562dd4476d7651e0d9739df12c40808d13dea
Klajdi660 <64031806+Klajdi660@users.noreply.github.com>
2022-04-22 13:49:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { Route, Switch } from 'react-router-dom';
-import FavoritePage from "./Pages/Favorites/Favorites";
+import Layout from './components/Layout/Layout';
 import MoviePage from "./Pages/MoviePage/MoviePage";
+import FavoritePage from "./Pages/Favorites/Favorites";
 import LaterPage from "./Pages/Later/Later";
 import Search from './Pages/Search/Search';
-import Layout from './components/Layout/Layout';
 
 const App = () => {
-
   return (
         <Layout>
           <Switch>
