ab6a6c2f90f8a9eb5a3824c94a9e39d1603d6ca1
Klajdi660 <64031806+Klajdi660@users.noreply.github.com>
2022-04-22 13:50:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
 import { FavoritesContextProvider } from "./store/favorites-context";
-
-import "./index.css";
-import App from "./App";
 import { LaterContextProvider } from "./store/later-context";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
     <FavoritesContextProvider>
